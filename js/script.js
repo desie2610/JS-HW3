df887306b0b0fc689b9df90b4299b57dd2595c11
desie2610 <168661973+desie2610@users.noreply.github.com>
2025-04-03 17:06:28 +0300

    asdasd

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const result = 5 + 5 + "5";
+
+console.log(typeof(result));
+
+// ==============
+
+const email = prompt("Введіть Email.");
+
+console.log(email.includes("@"));
+console.log(email.length);
+
+
+// ==============
+
+const w1 = "My";
+const w2 = "name";
+const w3 = "is";
+const w4 = "Victor";
+
+const fullName = w1 + w2 + w3 + w4;
+console.log(fullName);
+
+// ==============
+
+const userName = "Олександра";
+const payment = 300;
+
+alert (` Дякуємо, ${userName}! до сплати ${payment} гривень. `)
+
